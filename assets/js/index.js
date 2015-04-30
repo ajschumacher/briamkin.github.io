212973d0e716a3d1ef630568ef05df279e20b16c
briamkin <briam.kin@gmail.com>
2015-04-30 15:08:56 -0400

    edited hrefs to be target blank while filtering out in-page links

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,11 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
+        // On the home page, move the blog icon inside the header
         // for better relative/absolute positioning.
 
         //$("#blog-logo").prependTo("#site-head-content");
+        $("a[href^='http://'], a[href^='https://']").not(".header-link").attr("target", "_blank");
     });
 
 }(jQuery));
